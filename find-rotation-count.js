cb8fd68dfe2ae981c67d76de17de5a09f27fe900
mbouanik <mbouanik@student.42.fr>
2023-09-24 08:30:08 -0500

    find rotation count

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,17 @@
-function findRotationCount() {
-  
-}
+function findRotationCount(arr) {
+  let l = 0;
+  let r = arr.length - 1;
 
-module.exports = findRotationCount
+  while (l <= r) {
+    let m = Math.floor((l + r) / 2);
+    if (arr[m] > arr[m + 1]) {
+      return m + 1;
+    } else if (arr[l] > arr[m]) {
+      r = m - 1;
+    } else {
+      l = m + 1;
+    }
+  }
+  return 0;
+}
+module.exports = findRotationCount;
