46d56a2928be8827ec629554c1b3c0a9bf128315
mbouanik <mbouanik@student.42.fr>
2023-09-24 06:30:26 -0500

    sorted frequency

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,9 +1,40 @@
-function sortedFrequency(arr, num) {}
+function sortedFrequency(arr, num) {
+  let firstIn = firstInx(arr, num, 0, arr.length - 1);
+  if (firstIn === -1) {
+    return -1;
+  }
+  let lastInd = lastIndx(arr, num, 0, arr.length - 1);
+  if (lastInd === firstIn) {
+    return 1;
+  }
+  return lastInd - firstIn;
+}
+function firstInx(arr, num, l, r) {
+  while (l <= r) {
+    m = Math.floor((r + l) / 2);
+    if ((m === 0 || num > arr[m - 1]) && arr[m] === num) {
+      return m;
+    } else if (num > arr[m]) {
+      l = m + 1;
+    } else {
+      r = m - 1;
+    }
+  }
+  return -1;
+}
+
+function lastIndx(arr, num, l, r) {
+  while (l <= r) {
+    m = Math.floor((r + l) / 2);
+    if ((m === arr.length - 1 || num > arr[m + 1]) && arr[m] === m) {
+      return m;
+    } else if (num < arr[m]) {
+      r = m - 1;
+    } else {
+      l = m + 1;
+    }
+  }
+  return m;
+}
 
 module.exports = sortedFrequency;
-
-sortedFrequency([1, 1, 2, 2, 2, 2, 3], 2); // 4
-sortedFrequency([1, 1, 2, 2, 2, 2, 3], 3); // 1
-sortedFrequency([1, 1, 2, 2, 2, 2, 3], 1); // 2
-sortedFrequency([1, 1, 2, 2, 2, 2, 3], 4); // -1
-console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 2));
