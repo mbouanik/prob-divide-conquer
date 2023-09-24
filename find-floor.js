93f5b24643d5275ed23fd83945eedf3dd4247604
mbouanik <mbouanik@student.42.fr>
2023-09-24 08:47:49 -0500

    find floor

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,20 @@
-function findFloor() {
-  
+function findFloor(arr, num) {
+  let l = 0;
+  let r = arr.length - 1;
+  let res = -1;
+
+  while (l <= r) {
+    let m = Math.floor((l + r) / 2);
+    if (arr[m] <= num) {
+      res = arr[m];
+    }
+    if (arr[m] < num) {
+      l = m + 1;
+    } else if (arr[m] > num) {
+      r = m - 1;
+    }
+  }
+  return res;
 }
 
-module.exports = findFloor
+module.exports = findFloor;
